451ce895610123290819a8e65f768e505612ba2a
zzheng33 <zz695920754@gmail.com>
2025-08-29 18:11:50 +0000

    /

--- GPGPU/script/run_benchmark/ecp_script/.ipynb_checkpoints/miniFE-checkpoint.sh
+++ GPGPU/script/run_benchmark/ecp_script/.ipynb_checkpoints/miniFE-checkpoint.sh
@@ -5,4 +5,4 @@ benchmark_dir="/home/cc/benchmark/ECP/CPU-only/miniFE/openmp/src/"
 cd ${benchmark_dir}
 # mpirun -n 16 --use-hwthread-cpus ./miniFE.x  -nx 300 -ny 300 -nz 300
 # mpirun -n 48 --use-hwthread-cpus ./miniFE.x  -nx 528 -ny 512 -nz 512
-./miniFE.x  -nx 300 -ny 300 -nz 300
+./miniFE.x  -nx 500 -ny 500 -nz 500
